ba9bbf5efd4aa71b7149b3b22c92f21e8a81aae9
John Yam <mrjohnyam@gmail.com>
2015-04-16 15:09:08 -0400

    project1 complete
    NSFileManager

---
+++ Project2/Project2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by JOHN YAM on 4/16/15.
+//  Copyright (c) 2015 John Yam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
